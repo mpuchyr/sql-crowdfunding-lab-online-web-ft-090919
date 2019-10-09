3321864da496bf58d2d9c012d42ac1fec483ffcb
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 01:15:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT title, SUM(amount)
   FROM projects
-  OUTER JOIN projects.id = pledges.project_id
+  LEFT JOIN projects.id = pledges.project_id
   ORDER BY title;"
 end
 
